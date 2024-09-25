34c3c8339a48aeaf75deb9417150c4a03d39a2ad
Md Muktadir Nayem <mmuktadir629@gmail.com>
2024-09-25 15:29:23 +0600

    pages add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Collection from "./pages/Collection";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Orders from "./pages/Orders";
+import PlaceOrder from "./pages/PlaceOrder";
+import Product from "./pages/Product";
 
 
 const App = () => {
   return (
-    <div>
-      Hello world!
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9wv]">
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>}/>
+      <Route path="/cart" element={<Cart/>}/>
+      <Route path="/collection" element={<Collection/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/orders" element={<Orders/>}/>
+      <Route path="/place-order" element={<PlaceOrder/>}/>
+      <Route path="/product/:productId" element={<Product/>}/>
+    </Routes>
     </div>
   );
 };
