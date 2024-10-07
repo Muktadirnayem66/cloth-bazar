65a67093be3facdbe3783bbcf08f17741a56c888
Md Muktadir Nayem <mmuktadir629@gmail.com>
2024-10-07 21:08:59 +0600

    finished ui part and start the backend

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import cors from 'cors'
+import "dotenv/config"
+import ConnectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+
+
+//app config
+const app = express()
+const port = process.env.PORT || 4000
+ConnectDB()
+connectCloudinary()
+
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+//api endpoint
+app.get("", (req,res)=>{
+    res.send("Api working")
+})
+
+app.listen(port, ()=>{
+    console.log(`Server conected on port ${port}`);
+    
+})
+
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,34 @@
+import { assets } from "../assets/frontend_assets/assets";
+import NewsLetterBox from "../components/NewsLetterBox";
+import Title from "../components/Title";
 
 
 const Contact = () => {
     return (
         <div>
-            
+            <div className="text-center text-2xl pt-10 border-t">
+                <Title text1={"CONTACT"} text2={"US"}/>
+
+            </div>
+            <div className="flex my-10 flex-col justify-center md:flex-row gap-10 mb-28">
+                <img className="w-full md:max-w-[480px]" src={assets.contact_img} alt="contact_image" />
+                 <div className="flex flex-col justify-center items-start gap-6">
+                    <p className=" font-semibold text-2xl text-gray-600">Our Store</p>
+                    <p className="text-gray-500" >54709 Willms Station <br /> Suite 350, Washington, USA</p>
+                    
+                    <p className="text-gray-500" >Tel: (415) 555‑0132 <br />Email: greatstackdev@gmail.com</p>
+                    <p className=" font-semibold text-xl text-gray-600">Careers at Forever</p>
+                    <p className="text-gray-500">Learn more about our teams and job openings.</p>
+                    <button className="border rounded-sm transition-all duration-500 border-black  px-8 py-4 text-sm hover:bg-black hover:text-white">Explore Jobs</button>
+                 </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+            <NewsLetterBox/>
         </div>
     );
 };
