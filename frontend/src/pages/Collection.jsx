c2fde843d82af3f222dbfae8e52c41fe75d7d9bb
Md Muktadir Nayem <mmuktadir629@gmail.com>
2024-10-16 12:25:33 +0600

    fix frontend some issue

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { assets } from "../assets/frontend_assets/assets";
-import { ShopContext } from "../context/shopContext";
+import { ShopContext } from "../context/ShopContext.jsx";
 import Title from "../components/Title";
 import ProductItem from "../components/ProductItem";
 
