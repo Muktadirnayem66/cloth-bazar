d4d0bb3c1333e833d222f3aaa15307d6e9a1b2e4
Md Muktadir Nayem <mmuktadir629@gmail.com>
2024-10-16 11:51:40 +0600

    final working the e-commerce site

--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -1,19 +1,44 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { ShopContext } from "../context/ShopContext";
 import { useSearchParams } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 
 const Verify = () => {
 
     const {navigate, token, setCartItems, backendUrl} = useContext(ShopContext)
-    const  [searchParams, setSearchParams] = useSearchParams()
+    const  [searchParams] = useSearchParams()
 
     const success = searchParams.get("success")
     const orderId = searchParams.get("orderId")
 
     const verifyPayment = async()=>{
+        try {
+
+            if(!token){
+                return null
+            }
+            
+            const response = await axios.post(backendUrl + '/api/order/verifystripe', {success, orderId}, {headers:{token}})
+
+            if(response.data.success){
+                setCartItems({})
+                navigate("/orders")
+            }else{
+                navigate("/cart")
+            }
+
+        } catch (err) {
+            console.log(err);
+            toast.error(err.message)
+        }
 
     }
+
+    useEffect(()=>{
+        verifyPayment()
+    },[token])
 
     return (
         <div>
